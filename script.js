f691ba041391d6e603f208ab662e79a2c8b0e4a9
B0tta <luca.bottani00@gmail.com>
2023-06-19 17:37:10 +0200

    download pdf

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("downloadLink").addEventListener("click", function () {
+  var link = document.createElement("a");
+  link.href = "assets/TileTalks.pdf";
+  link.download = "TileTalks.pdf";
+  link.click();
+});
