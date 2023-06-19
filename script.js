54c21199cad69bfb1e39e4e5876e1ff545b3f125
B0tta <luca.bottani00@gmail.com>
2023-06-19 19:40:12 +0200

    deleted sketch

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("downloadLink").addEventListener("click", function () {
-  var link = document.createElement("a");
-  link.href = "assets/TileTalks.pdf";
-  link.download = "TileTalks.pdf";
-  link.click();
-});
-//<script src="script.js"></script>
